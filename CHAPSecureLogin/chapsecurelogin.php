d6f15819d66ee1a10454265c612dd3a4facc9869
redsend <redsend@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-02 13:43:14 +0000

    tagging again version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/chap-secure-login/tags/1.5@115010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- CHAPSecureLogin/chapsecurelogin.php
+++ CHAPSecureLogin/chapsecurelogin.php
@@ -8,7 +8,7 @@ Author: Enrico Rossomando (redsend)
 Author URI: http://www.redsend.org
 */
 
-/*  Copyright 2007  Enrico Rossomando (email : redsend@gmail.com)
+/*  Copyright 2007-2009  Enrico Rossomando (email : redsend@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
